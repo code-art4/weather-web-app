9a5a6b9ae787571a0dc623248db07181af29863d
code-art4 <ojotriumph4@gmail.com>
2022-02-25 07:33:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+
 
 function App() {
+
+  // const url = "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=1ee3fa6552c3ceddeb7cf905b19a4088"
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <div className="container">
+          <div className="top">
+            <div className="location">
+              <p>Dallas</p>
+            </div>
+            <div className="temp">
+              <h1>60&deg;F</h1>
+            </div>
+            <div className="description">
+              <p>Clouds</p>
+            </div>
+          </div>
+          <div className="bottom">
+            <div className="feels">
+              <p>65&deg;F</p>
+            </div>
+
+            <div className="humidity">
+              <p>20%</p>
+            </div>
+
+            <div className="wind">
+              12 MPH
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
